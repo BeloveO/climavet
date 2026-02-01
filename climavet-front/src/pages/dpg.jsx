956483a82fa4aeeca7720a16611b2e75d882fbe2
Belove <beloveolusola@gmail.com>
2026-02-01 04:05:29 -0700

    Initial Blueprint

---
+++ climavet-front/src/pages/dpg.jsx
@@ -0,0 +1,9 @@
+function Dpg() {
+    return (
+        <div>
+            <h1>Disaster Plan Generator</h1>
+            <p>This tool assists veterinary clinics in creating customized disaster preparedness plans to effectively respond to climate-related emergencies.</p>
+        </div>
+    )
+}
+export default Dpg
---
+++ climavet-front/src/pages/raw.jsx
@@ -0,0 +1,9 @@
+function Raw() {
+    return (
+        <div>
+            <h1>Risk Assessment Wizard</h1>
+            <p>This tool will guide through assessing any climate-related risk your clinic could face</p>
+        </div>
+    )
+}
+export default Raw
